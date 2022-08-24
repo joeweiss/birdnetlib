06dac5022f1c9de7d03d2980f9b7ee81769d6e60
Joe Weiss <joe.weiss@gmail.com>
2022-08-24 17:13:37 -0400

    Add examples for batch.DirectoryAnalyzer (#29)

---
+++ examples/batch_directory.py
@@ -0,0 +1,34 @@
+from birdnetlib.batch import DirectoryAnalyzer
+from birdnetlib.analyzer_lite import LiteAnalyzer
+from datetime import datetime
+from pprint import pprint
+
+
+def on_analyze_complete(recording):
+    print(recording.path)
+    pprint(recording.detections)
+
+
+def on_error(recording, error):
+    print("An exception occurred: {}".format(error))
+    print(recording.path)
+
+
+print("Starting Analyzer")
+analyzer = LiteAnalyzer()
+
+
+print("Starting Watcher")
+directory = "."
+batch = DirectoryAnalyzer(
+    directory,
+    analyzers=[analyzer],
+    lon=-120.7463,
+    lat=35.4244,
+    date=datetime(year=2022, month=5, day=10),
+    min_conf=0.4,
+)
+
+batch.on_analyze_complete = on_analyze_complete
+batch.on_error = on_error
+batch.process()
