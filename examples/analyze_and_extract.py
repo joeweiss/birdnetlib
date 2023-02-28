8f6d5bab1f10e75009a548e152f5d9791ce5869a
Joe Weiss <joe.weiss@gmail.com>
2023-02-28 13:38:19 -0500

    Fix comments in extraction example

--- examples/analyze_and_extract.py
+++ examples/analyze_and_extract.py
@@ -27,13 +27,13 @@ pprint(recording.detections)
 [{'common_name': 'House Finch',
   'confidence': 0.5066996216773987,
   'end_time': 12.0,
-  'extraction_path': 'extractions/2022-08-15-birdnet-21:05:54_7s-14s.flac',
+  'extraction_path': 'extractions/2022-08-15-birdnet-21:05:54_9s-12s.flac',
   'scientific_name': 'Haemorhous mexicanus',
   'start_time': 9.0},
  {'common_name': 'Dark-eyed Junco',
   'confidence': 0.3555494546890259,
   'end_time': 36.0,
-  'extraction_path': 'extractions/2022-08-15-birdnet-21:05:54_31s-38s.flac',
+  'extraction_path': 'extractions/2022-08-15-birdnet-21:05:54_33s-36s.flac',
   'scientific_name': 'Junco hyemalis',
   'start_time': 33.0}
  ]
@@ -51,9 +51,9 @@ recording = Recording(
 recording.analyze()
 export_dir = "extractions"  # Directory should already exist.
 
-# Extract to .mp3 audio files, only if confidence is > 0.5 with no seconds of audio padding.
+# Extract to .mp3 audio files, only if confidence is > 0.5 with 2 seconds of audio padding.
 recording.extract_detection_as_audio(
-    directory=export_dir, format="mp3", bitrate="192k", min_conf=0.5, padding_secs=0
+    directory=export_dir, format="mp3", bitrate="192k", min_conf=0.5, padding_secs=2
 )
 
 pprint(recording.detections)
